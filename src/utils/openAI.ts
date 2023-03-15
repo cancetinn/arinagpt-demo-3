c4f294291b633aa42c38508500779cfafdeb8c4f
Can Çetin <75146291+cancetinn@users.noreply.github.com>
2023-03-15 16:09:56 +0300

    Update openAI.ts

--- src/utils/openAI.ts
+++ src/utils/openAI.ts
@@ -10,7 +10,7 @@ export const generatePayload = (apiKey: string, messages: ChatMessage[]): Reques
   body: JSON.stringify({
     model: 'gpt-3.5-turbo-0301',
     messages,
-    temperature: 0.6,
+    temperature: 0,
     stream: true,
   }),
 })
